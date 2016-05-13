5ba73dfa1970a6f9b02e85a6df481b12b04ad28e
LarsBq <lars.g.bergqvist@gmail.com>
2016-05-13 14:57:20 +0200

    First simple Flask app to get a GUI in place

---
+++ RemoteControlApp/remotecontrol.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+
+buttonToIDCodesMap = { 
+1: 0x15,
+2: 0x45,
+3: 0x51,
+4: 0x54
+}
+
+byte0codeON = 0x54
+byte0codeOFF = 0x55
+
+byte2 = 0x15
+
+app = Flask(__name__)
+
+@app.route("/button/<int:buttonNumber>/<string:state>",methods=['GET'])
+def clickButton(buttonNumber,state):
+    print(buttonNumber)
+    print(state)
+    idcode = buttonToIDCodesMap[buttonNumber]
+    byte0code = byte0codeON
+    if state == 'off':
+        byte0code = byte0codeOFF
+    code = (byte2 << 16) | (idcode << 8) | byte0code
+    print(format(code,'000000x'))
+    return redirect(url_for('start'))
+
+@app.route("/")
+def start():
+    return render_template('rcremote.html')
+
+if __name__ == "__main__":
+#    app.debug = True
+    app.run(host='0.0.0.0',port=8080)
