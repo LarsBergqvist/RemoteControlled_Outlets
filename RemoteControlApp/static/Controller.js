8ea116a6d9b8800b0446e93485158e684fed6543
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-05-14 21:40:17 +0200

    Added names on buttons

--- RemoteControlApp/static/Controller.js
+++ RemoteControlApp/static/Controller.js
@@ -4,18 +4,16 @@ myApp.controller('OutletController', ['$scope', '$http', function($scope, $http)
 
     $scope.header = 'Outlets';
   
-    var outlet1 = {id:"1"};
-    var outlet2 = {id:"2"};
-    var outlet3 = {id:"3"};
-    var outlet4 = {id:"4"};
+    var outlet1 = {id:"1", name:"Spotlights"};
+    var outlet2 = {id:"2", name:"Stereo"};
+    var outlet3 = {id:"3", name:"Table lamp"};
+    var outlet4 = {id:"4", name:"Floor lamp"};
     $scope.outlets = {outlet1,outlet2,outlet3,outlet4};
   
     $scope.pressButton = function(id,action) {
-//        alert(id + action);
         $http.put("/Outlets/api/outlet/"+id, { state : action}).then(function(response) {
         }, function(error) {}
         );
-
     }
   
 }]);
